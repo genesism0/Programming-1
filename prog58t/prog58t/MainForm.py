f89699befee5bb20a2c0d592a5570ed57ceef5ab
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-03-11 10:27:40 -0500

    3/11

--- prog58t/prog58t/MainForm.py
+++ prog58t/prog58t/MainForm.py
@@ -267,9 +267,10 @@ class MainForm(Form):
 		Change = Amount - Price
 		Dollars = change / 1
 		Dollars = round(Dollars, 1)
-			
-			
+		Quarters = change - Dollars
+		Quarters = Quarters / .25
 		Dimes = 
+		Dimes = Dimes / 
 		Nickles = 
 		Pennies = 
 		
