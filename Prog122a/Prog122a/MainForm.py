8609dca3b0d569108e2f4440272d3a7131c91131
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-03-25 10:09:28 -0500

    3/25

---
+++ Prog122a/Prog122a/MainForm.py
@@ -0,0 +1,84 @@
+﻿import math
+import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(7, 7)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(349, 199)
+		self._listBox1.TabIndex = 0
+		self._listBox1.SelectedIndexChanged += self.ListBox1SelectedIndexChanged
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(7, 212)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(117, 47)
+		self._button1.TabIndex = 1
+		self._button1.Text = "button1"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Location = System.Drawing.Point(126, 212)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(118, 47)
+		self._button2.TabIndex = 2
+		self._button2.Text = "button2"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Location = System.Drawing.Point(246, 212)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(110, 47)
+		self._button3.TabIndex = 3
+		self._button3.Text = "button3"
+		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(364, 264)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "Prog122a"
+		self.ResumeLayout(False)
+
+
+	# put "import math" as line 1 of the Program
+	def Button1Click(self, sender, e):
+		heading = "Number\t\tSquare\t\tSquare Root"
+		self._listBox1.Items.Add(heading)
+		for num in range(1, 50+1):
+			nsqrd = num**2
+			nsqrt = math.sqrt(num)
+			msg = str(num) + "\t\t" + str(nsqrs) + "\t\t" + str(round(nsqrt, 4))
+			self._listBox1.Items.Add(msg)
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Claer()
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
