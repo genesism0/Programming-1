31ceed63d00c5deeab51e739ca1e0e538075cd30
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-04-02 10:30:09 -0500

    4/2

--- ptog93a/ptog93a/MainForm.py
+++ ptog93a/ptog93a/MainForm.py
@@ -22,41 +22,50 @@ class MainForm(Form):
 		self._label7 = System.Windows.Forms.Label()
 		self._label8 = System.Windows.Forms.Label()
 		self._label9 = System.Windows.Forms.Label()
+		self._label10 = System.Windows.Forms.Label()
+		self._label11 = System.Windows.Forms.Label()
 		self.SuspendLayout()
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(7, 205)
+		self._button1.BackColor = System.Drawing.Color.LavenderBlush
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(228, 6)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(78, 51)
+		self._button1.Size = System.Drawing.Size(136, 57)
 		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(91, 205)
+		self._button2.BackColor = System.Drawing.Color.HotPink
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(228, 73)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(75, 51)
+		self._button2.Size = System.Drawing.Size(136, 55)
 		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(172, 205)
+		self._button3.BackColor = System.Drawing.Color.Plum
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(228, 139)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(79, 51)
+		self._button3.Size = System.Drawing.Size(136, 55)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.UseVisualStyleBackColor = False
 		self._button3.Click += self.Button3Click
 		# 
 		# label1
 		# 
-		self._label1.Location = System.Drawing.Point(12, 9)
+		self._label1.BackColor = System.Drawing.Color.Pink
+		self._label1.Location = System.Drawing.Point(7, 6)
 		self._label1.Name = "label1"
 		self._label1.Size = System.Drawing.Size(100, 23)
 		self._label1.TabIndex = 3
@@ -64,78 +73,103 @@ class MainForm(Form):
 		# 
 		# textBox1
 		# 
-		self._textBox1.Location = System.Drawing.Point(151, 9)
+		self._textBox1.Location = System.Drawing.Point(118, 6)
+		self._textBox1.Multiline = True
 		self._textBox1.Name = "textBox1"
-		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.Size = System.Drawing.Size(104, 23)
 		self._textBox1.TabIndex = 4
 		# 
 		# label2
 		# 
-		self._label2.Location = System.Drawing.Point(12, 53)
+		self._label2.BackColor = System.Drawing.Color.Pink
+		self._label2.Location = System.Drawing.Point(7, 40)
 		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(133, 23)
+		self._label2.Size = System.Drawing.Size(100, 23)
 		self._label2.TabIndex = 5
 		self._label2.Text = "Base Rate:"
 		# 
 		# label3
 		# 
-		self._label3.Location = System.Drawing.Point(151, 53)
+		self._label3.BackColor = System.Drawing.Color.PaleVioletRed
+		self._label3.Location = System.Drawing.Point(118, 40)
 		self._label3.Name = "label3"
 		self._label3.Size = System.Drawing.Size(100, 23)
 		self._label3.TabIndex = 6
-		self._label3.Text = "label3"
 		# 
 		# label4
 		# 
-		self._label4.Location = System.Drawing.Point(9, 99)
+		self._label4.BackColor = System.Drawing.Color.Pink
+		self._label4.Location = System.Drawing.Point(7, 73)
 		self._label4.Name = "label4"
-		self._label4.Size = System.Drawing.Size(112, 23)
+		self._label4.Size = System.Drawing.Size(100, 23)
 		self._label4.TabIndex = 7
-		self._label4.Text = "Citytax:"
+		self._label4.Text = "Surcharge:"
 		# 
 		# label5
 		# 
-		self._label5.Location = System.Drawing.Point(142, 99)
+		self._label5.BackColor = System.Drawing.Color.PaleVioletRed
+		self._label5.Location = System.Drawing.Point(118, 73)
 		self._label5.Name = "label5"
 		self._label5.Size = System.Drawing.Size(100, 23)
 		self._label5.TabIndex = 8
-		self._label5.Text = "label5"
 		# 
 		# label6
 		# 
-		self._label6.Location = System.Drawing.Point(9, 137)
+		self._label6.BackColor = System.Drawing.Color.Pink
+		self._label6.Location = System.Drawing.Point(7, 105)
 		self._label6.Name = "label6"
 		self._label6.Size = System.Drawing.Size(100, 23)
 		self._label6.TabIndex = 9
-		self._label6.Text = "Pay this amount:"
+		self._label6.Text = "Citytax:"
 		# 
 		# label7
 		# 
-		self._label7.Location = System.Drawing.Point(155, 137)
+		self._label7.BackColor = System.Drawing.Color.PaleVioletRed
+		self._label7.Location = System.Drawing.Point(118, 105)
 		self._label7.Name = "label7"
 		self._label7.Size = System.Drawing.Size(100, 23)
 		self._label7.TabIndex = 10
-		self._label7.Text = "label7"
 		# 
 		# label8
 		# 
-		self._label8.Location = System.Drawing.Point(7, 171)
+		self._label8.BackColor = System.Drawing.Color.Pink
+		self._label8.Location = System.Drawing.Point(7, 137)
 		self._label8.Name = "label8"
 		self._label8.Size = System.Drawing.Size(100, 23)
 		self._label8.TabIndex = 11
-		self._label8.Text = "After May 20th Pay:"
+		self._label8.Text = "Pay this amount:"
 		# 
 		# label9
 		# 
-		self._label9.Location = System.Drawing.Point(142, 171)
+		self._label9.BackColor = System.Drawing.Color.PaleVioletRed
+		self._label9.Location = System.Drawing.Point(118, 137)
 		self._label9.Name = "label9"
 		self._label9.Size = System.Drawing.Size(100, 23)
 		self._label9.TabIndex = 12
-		self._label9.Text = "label9"
+		# 
+		# label10
+		# 
+		self._label10.BackColor = System.Drawing.Color.Pink
+		self._label10.Location = System.Drawing.Point(7, 171)
+		self._label10.Name = "label10"
+		self._label10.Size = System.Drawing.Size(100, 23)
+		self._label10.TabIndex = 13
+		self._label10.Text = "After May 20th Pay:"
+		# 
+		# label11
+		# 
+		self._label11.BackColor = System.Drawing.Color.PaleVioletRed
+		self._label11.Location = System.Drawing.Point(118, 171)
+		self._label11.Name = "label11"
+		self._label11.Size = System.Drawing.Size(100, 23)
+		self._label11.TabIndex = 14
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(267, 258)
+		self.BackColor = System.Drawing.Color.Thistle
+		self.ClientSize = System.Drawing.Size(376, 206)
+		self.Controls.Add(self._label11)
+		self.Controls.Add(self._label10)
 		self.Controls.Add(self._label9)
 		self.Controls.Add(self._label8)
 		self.Controls.Add(self._label7)
@@ -156,11 +190,26 @@ class MainForm(Form):
 
 
 	def Button1Click(self, sender, e):
-		Kilowatts = int(self._textBox.Text)
-		Kilowatts*4.75
+		Kilowatts = int(self._textBox1.Text)
+		baseR = Kilowatts  *4.75
+		surcharge = baseR * 0.1
+		cityT = baseR * 0.03
+		total = baseR + surcharge + cityT
+		lateF = total * 0.04
+		
+		self._label3.Text = float(baseR)
+		self._label5.Text = float(surcharge)
+		self._label7.Text = float(cityT)
+		self._label9.Text = float(total)
+		self._label11.Text = float(lateF)
 
 	def Button2Click(self, sender, e):
-		pass
+		self._textBox1.Text = ""
+		self._label3.Text = ""
+		self._label5.Text = ""
+		self._label7.Text = ""
+		self._label9.Text = ""
+		self._label11.Text = ""
 
 	def Button3Click(self, sender, e):
 		Application.Exit()
