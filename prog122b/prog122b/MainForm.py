9a1bf7d0616a8bb8b501299813ba2ed94be43be0
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-04-08 10:29:34 -0500

    4/8

---
+++ prog122b/prog122b/MainForm.py
@@ -0,0 +1,76 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(12, 236)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(89, 48)
+		self._button1.TabIndex = 0
+		self._button1.Text = "button1"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Location = System.Drawing.Point(107, 236)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(75, 23)
+		self._button2.TabIndex = 1
+		self._button2.Text = "button2"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Location = System.Drawing.Point(206, 236)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(75, 23)
+		self._button3.TabIndex = 2
+		self._button3.Text = "button3"
+		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(269, 199)
+		self._listBox1.TabIndex = 3
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(444, 334)
+		self.Controls.Add(self._listBox1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "prog122b"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		heading = "Hours"   "Pay"
+		self._listBox1.Add(heading)
+
+	def Button2Click(self, sender, e):
+		pass
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
