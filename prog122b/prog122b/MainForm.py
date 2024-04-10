53646f41fd7bd09b9add96cc1a15eaa5168c8a0e
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-04-10 10:47:25 -0500

    4/10

--- prog122b/prog122b/MainForm.py
+++ prog122b/prog122b/MainForm.py
@@ -17,31 +17,31 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(12, 236)
+		self._button1.Location = System.Drawing.Point(287, 12)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(89, 48)
+		self._button1.Size = System.Drawing.Size(145, 60)
 		self._button1.TabIndex = 0
-		self._button1.Text = "button1"
+		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = True
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(107, 236)
+		self._button2.Location = System.Drawing.Point(287, 88)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(75, 23)
+		self._button2.Size = System.Drawing.Size(145, 60)
 		self._button2.TabIndex = 1
-		self._button2.Text = "button2"
+		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = True
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(206, 236)
+		self._button3.Location = System.Drawing.Point(287, 164)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(75, 23)
+		self._button3.Size = System.Drawing.Size(145, 60)
 		self._button3.TabIndex = 2
-		self._button3.Text = "button3"
+		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = True
 		self._button3.Click += self.Button3Click
 		# 
@@ -50,12 +50,12 @@ class MainForm(Form):
 		self._listBox1.FormattingEnabled = True
 		self._listBox1.Location = System.Drawing.Point(12, 12)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(269, 199)
+		self._listBox1.Size = System.Drawing.Size(269, 212)
 		self._listBox1.TabIndex = 3
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(444, 334)
+		self.ClientSize = System.Drawing.Size(444, 231)
 		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
@@ -66,8 +66,8 @@ class MainForm(Form):
 
 
 	def Button1Click(self, sender, e):
-		heading = "Hours"   "Pay"
-		self._listBox1.Add(heading)
+		Heading = "Hours"   "Pay"
+		self._listBox1.Text(heading)
 
 	def Button2Click(self, sender, e):
 		pass
