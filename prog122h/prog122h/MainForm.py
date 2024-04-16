e83497b7d0eb88654d09703e54402c58e852e5ed
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-04-16 10:30:05 -0500

    4/16

--- prog122h/prog122h/MainForm.py
+++ prog122h/prog122h/MainForm.py
@@ -1,4 +1,5 @@
-﻿import System.Drawing
+﻿import math
+import System.Drawing
 import System.Windows.Forms
 
 from System.Drawing import *
@@ -17,45 +18,50 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
+		self._listBox1.BackColor = System.Drawing.SystemColors.HotTrack
 		self._listBox1.FormattingEnabled = True
-		self._listBox1.Location = System.Drawing.Point(34, 12)
+		self._listBox1.Location = System.Drawing.Point(12, 12)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(243, 134)
+		self._listBox1.Size = System.Drawing.Size(343, 121)
 		self._listBox1.TabIndex = 0
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(12, 173)
+		self._button1.BackColor = System.Drawing.Color.LightSkyBlue
+		self._button1.Location = System.Drawing.Point(12, 139)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.Size = System.Drawing.Size(98, 32)
 		self._button1.TabIndex = 1
-		self._button1.Text = "button1"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(109, 173)
+		self._button2.BackColor = System.Drawing.Color.LightSkyBlue
+		self._button2.Location = System.Drawing.Point(138, 138)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(83, 25)
+		self._button2.Size = System.Drawing.Size(98, 33)
 		self._button2.TabIndex = 2
-		self._button2.Text = "button2"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(202, 174)
+		self._button3.BackColor = System.Drawing.Color.LightSkyBlue
+		self._button3.Location = System.Drawing.Point(257, 139)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(75, 23)
+		self._button3.Size = System.Drawing.Size(98, 32)
 		self._button3.TabIndex = 3
-		self._button3.Text = "button3"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
 		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(322, 252)
+		self.BackColor = System.Drawing.Color.DodgerBlue
+		self.ClientSize = System.Drawing.Size(367, 176)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
@@ -69,11 +75,18 @@ class MainForm(Form):
 		heading = "Number" + "\t" "Square" + "\t" "Square Root" + "\t" "Cube" + "\t" "4th Root"
 		self._listBox1.Items.Add(heading)
 		for num in range(1, 20+1):
-			square = num*num
-			sq = 
+			square = num**2
+			sq = math.sqrt(num)
+			sq = round(sq, 4)
+			cube = num**3
+			Froot = sq**4
+			Froot = round(Froot, 4)
+			cal = str(num) + "\t" + str(square) + "\t" + str(sq) + "\t" + str(cube) + "\t" + str(Froot)
+			self._listBox1.Items.Add(cal)
+			
 
 	def Button2Click(self, sender, e):
-		self.listBox1.Items.Clear()
+		self._listBox1.Items.Clear()
 
 	def Button3Click(self, sender, e):
 		Application.Exit()
