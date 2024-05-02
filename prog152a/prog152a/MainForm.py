8034a631b08d590e492798f018d3743e2b96aacc
genesism0 <162478495+genesism0@users.noreply.github.com>
2024-05-02 10:29:19 -0500

    5/2

--- prog152a/prog152a/MainForm.py
+++ prog152a/prog152a/MainForm.py
@@ -17,56 +17,66 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
+		self._listBox1.BackColor = System.Drawing.Color.LightPink
 		self._listBox1.FormattingEnabled = True
-		self._listBox1.Location = System.Drawing.Point(2, 2)
+		self._listBox1.Location = System.Drawing.Point(4, 4)
 		self._listBox1.Name = "listBox1"
 		self._listBox1.Size = System.Drawing.Size(182, 251)
 		self._listBox1.TabIndex = 0
+		
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(190, 2)
+		self._button1.BackColor = System.Drawing.Color.DarkOrange
+		self._button1.Font = System.Drawing.Font("Modern No. 20", 21.7499981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(190, 4)
 		self._button1.Name = "button1"
 		self._button1.Size = System.Drawing.Size(159, 74)
 		self._button1.TabIndex = 1
-		self._button1.Text = "button1"
-		self._button1.UseVisualStyleBackColor = True
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
+		self._button2.BackColor = System.Drawing.Color.DarkOrange
+		self._button2.Font = System.Drawing.Font("Modern No. 20", 21.7499981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._button2.Location = System.Drawing.Point(190, 92)
 		self._button2.Name = "button2"
 		self._button2.Size = System.Drawing.Size(159, 74)
 		self._button2.TabIndex = 2
-		self._button2.Text = "button2"
-		self._button2.UseVisualStyleBackColor = True
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
 		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(190, 178)
+		self._button3.BackColor = System.Drawing.Color.DarkOrange
+		self._button3.Font = System.Drawing.Font("Modern No. 20", 21.7499981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(190, 181)
 		self._button3.Name = "button3"
 		self._button3.Size = System.Drawing.Size(159, 74)
 		self._button3.TabIndex = 3
-		self._button3.Text = "button3"
-		self._button3.UseVisualStyleBackColor = True
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
 		self._button3.Click += self.Button3Click
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(378, 316)
+		self.BackColor = System.Drawing.Color.FloralWhite
+		self.ClientSize = System.Drawing.Size(350, 263)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
 		self.Controls.Add(self._listBox1)
 		self.Name = "MainForm"
 		self.Text = "prog152a"
+	
 		self.ResumeLayout(False)
 
 
 	def Button1Click(self, sender, e):
-		for num in range(3, 9669+1):
+		for num in range (3, 9669, 3):
 			sum = num/3
 			out = str(sum)
 			self._listBox1.Items.Add(out)
